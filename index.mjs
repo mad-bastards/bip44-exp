7203e10d5ca1ac411bf49191eee5e84bd6ea8bd3
nobody <github2@invisiblehand.church>
2022-11-03 02:46:44 -0400

    x

--- index.mjs
+++ index.mjs
@@ -8,6 +8,16 @@ import CoinKey from 'coinkey';
 import coininfo from 'coininfo';
 
 
+const Coinomi = {
+  "BCH" : "qqqrq9706mttv5h8vvcw82ze63ua38y22y36798xn8",
+  "BTC" : "1JoP2wkKLTHk6GR1oUsVc7hJU1AXaTLLH7",
+  "DASH" : "XwYE6phFnaDJgJjD9bRigx4btc98tWw7dg",
+  "DOGE" : "DFeVnKxWT4pDf6sX15HYHtN9vEwbKuRoXr",
+  "LBC" : "lbc1q8pywc7k0s05mvzd4pl6kmp76d98map57cf3mqv",
+  "XMR" : "48Rq4Ga13UqgKSooN6EqpzDzroS1zUjyMTdUgeteup3ihYcb3gzeb8Xhx7q5j83wB5iEbgVqgsDJyLXAvNw6KiD19v6Pwi2",
+  "XRP" : "r4joJp9ZuDHJ1RJAd1bU84BnS7w7mHkXaP",
+  "ZEN" : "znXw56vq7n6q92gTkk1VJutWpJppXP84BQW"
+};
 
 global.pp=function pp(val) { return JSON.stringify(val,null,2); };
 global.log=function log(val) { console.log(val); };
@@ -16,7 +26,8 @@ global.dump=function dump(val) { return log(pp(val)); };
 function Coin(sym){
   const info=coininfo(sym);
   if(info==null)
-    return null;
+    throw new Error(`no coininfo for "${sym}"`);
+
   const versions=info.versions;
 
   this.sym=sym;
@@ -30,7 +41,6 @@ function Coin(sym){
     const coinKey = new CoinKey(child.privateKey,coininfo(sym));
     const addr = coinKey.publicAddress;
     this.addrs.push(addr);
-
   }
 }
 function Wallet(...syms){
@@ -44,6 +54,7 @@ global.phrase= await getPhrase();
 global.seed = bip39.mnemonicToSeedSync(phrase);
 global.hdkey = HDKey.fromMasterSeed(seed)
 global.keys=Object.keys;
-const wallet = new Wallet('BTC','BCH','DOGE','ZEN');
+
+const wallet = new Wallet('BCH','BTC',"DASH",'DOGE',"LBC","XMR","XRP",'ZEN');
 console.log(wallet);
 
