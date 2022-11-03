9518e6384375fd9115909de89c44db41f82088ab
nobody <github2@invisiblehand.church>
2022-11-03 02:07:41 -0400

    x

--- index.mjs
+++ index.mjs
@@ -2,10 +2,10 @@ import HDKey from 'hdkey'
 import { getPhrase } from './util.mjs';
 import bip39 from 'bip39';
 import { encode as bs58e, decode as bs58d } from 'bs58';
+import bip44c from 'bip44-constants';
 const assert = (await import('assert')).default;
 import CoinKey from 'coinkey';
 import coininfo from 'coininfo';
-import bip44c from 'bip44-constants';
 
 
 
@@ -13,10 +13,6 @@ global.pp=function pp(val) { return JSON.stringify(val,null,2); };
 global.log=function log(val) { console.log(val); };
 global.dump=function dump(val) { return log(pp(val)); };
 
-global.phrase= await getPhrase();
-global.seed = bip39.mnemonicToSeedSync(phrase);
-global.hdkey = HDKey.fromMasterSeed(seed)
-const ci={};
 function Coin(sym){
   const info=coininfo(sym);
   if(info==null)
@@ -27,12 +23,15 @@ function Coin(sym){
   this.versions=versions;
   this.bip44=versions.bip44;
   this.path=`m/44'/${this.bip44}'/0'/0`;
-  this.root=hdkey.derive(this.path);
+  this.acct=hdkey.derive(this.path);
   this.addrs=[];
   for(var i=0;i<3;i++){
-    const child = this.root.deriveChild(i);
-    this.addrs.push(new CoinKey(child.privateKey,coininfo(sym)).publicAddress);
-  };
+    const child = this.acct.deriveChild(i);
+    const coinKey = new CoinKey(child.privateKey,coininfo(sym));
+    const addr = coinKey.publicAddress;
+    this.addrs.push(addr);
+
+  }
 }
 function Wallet(...syms){
   this.coins={};
@@ -40,6 +39,11 @@ function Wallet(...syms){
     this.coins[sym]=new Coin(sym);
   });
 }
+
+global.phrase= await getPhrase();
+global.seed = bip39.mnemonicToSeedSync(phrase);
+global.hdkey = HDKey.fromMasterSeed(seed)
 global.keys=Object.keys;
-dump(new Wallet('BTC','BCH','DOGE','ZEN'));
+const wallet = new Wallet('BTC','BCH','DOGE','ZEN');
+console.log(wallet);
 
